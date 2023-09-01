ed10541611b5735592221daaf9c2cec73511c951
G10H4rDy <132031977+G10H4rDy@users.noreply.github.com>
2023-08-31 22:33:26 -0400

    Time: 104 ms (33.4%), Space: 41.9 MB (87.7%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,10 +1,20 @@
 var twoSum = function(nums, target) {
-  
-   let obj ={};
-   for (let i = 0; i < nums.length; i++){
-       if(obj[target - nums[i]] >= 0){
-           return [obj[target - nums[i]],i];
-       }
-       obj[nums[i]] = i;
-   }
-};
+  const nums2 = [];
+
+for (let i = 0; i < nums.length; i++){
+  if (nums2.includes(nums[i])){
+      return [nums2.indexOf(nums[i]), i]
+    }else{
+      nums2.push(target - nums[i])
+    }
+}
+
+
+  // for(element of nums){
+  //   if (nums2.includes(element)){
+  //     return [nums.indexOf(target - element), nums.indexOf(element)]
+  //   }else{
+  //     nums2.push(target - element)
+  //   }
+  // }
+}   
