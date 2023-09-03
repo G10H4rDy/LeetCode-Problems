6b42a2a1d9b2a3bc16b837350ed8789973c3cf42
G10H4rDy <132031977+G10H4rDy@users.noreply.github.com>
2023-09-02 22:26:14 -0400

    Time: 5720 ms (8.91%), Space: 54.2 MB (53.91%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,11 +3,11 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const nums1 = []
+    const nums1 = {}
     
     for (let num of nums){
-        if (nums1.includes(num)) return true
-        else nums1.push(num)
+        if (nums1[num] >=0) return true
+        else nums1[num] = nums.indexOf(num)
     }
     return false
 };
