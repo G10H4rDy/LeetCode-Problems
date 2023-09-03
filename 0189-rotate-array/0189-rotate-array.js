76fa22d6d1c0b1e959f8c75321112e5ff147a0f9
G10H4rDy <132031977+G10H4rDy@users.noreply.github.com>
2023-09-02 23:08:39 -0400

    Time: 639 ms (5.02%), Space: 52.5 MB (46.65%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+
+    const nums1 = [];
+
+    if (k >= nums.length) {
+        k = k % nums.length
+        if (k === 0){
+            return nums
+        }
+    }
+
+    for (let i = 0; i < k ; i++){
+        nums1.push(nums.pop())
+    }
+
+     for (let i = 0; i < nums1.length ; i++){
+        nums.unshift(nums1[i])
+    }
+};
