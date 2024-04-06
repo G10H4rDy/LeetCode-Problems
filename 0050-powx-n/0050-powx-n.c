6e7cbac882a0c5580947f677e085e54bad231699
G10H4rDy <132031977+G10H4rDy@users.noreply.github.com>
2024-04-06 00:05:11 -0400

    Time: 4 ms (11.6%), Space: 5.6 MB (46.54%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.c
@@ -0,0 +1,19 @@
+double myPow(double x, int n) {
+        if (n == 0) return 1;
+    if (n == 1) return x;
+    if (n < 0) {
+        // Handle potential overflow when n == INT_MIN
+        if (n == INT_MIN) {
+            return 1 / (myPow(x, INT_MAX) * x);
+        } else {
+            return 1 / myPow(x, -n);
+        }
+    }
+    double half = myPow(x, n / 2);
+    if (n % 2 == 0) {
+        return half * half;
+    } else {
+        return half * half * x;
+    }
+
+}
