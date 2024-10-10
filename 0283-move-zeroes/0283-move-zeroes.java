8dabbe67ce89931e74b3d19e71f232ad9c8f34cf
G10H4rDy <132031977+G10H4rDy@users.noreply.github.com>
2024-10-10 15:04:48 -0400

    Time: 3 ms (24.53%), Space: 45.9 MB (33.7%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,11 +3,16 @@ class Solution {
       // Start with two pointers 
       int left = 0;
       int pointer = 0;
+
+    //  Go through array
       while(pointer <= nums.length- 1){
+        // If you encounter non-zero value
       if (nums[pointer] != 0){
+        if (left != pointer){
         int copy = nums[left];
         nums[left] = nums[pointer];
         nums[pointer] = copy;
+        }
         left++;
       }
       pointer++;
