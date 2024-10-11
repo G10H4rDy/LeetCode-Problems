41f7cd0e36655eb7be218e9f0d9e4a6519c037bc
G10H4rDy <132031977+G10H4rDy@users.noreply.github.com>
2024-10-10 20:38:07 -0400

    Time: 1 ms (99.43%), Space: 57.2 MB (30.07%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,34 +1,28 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int biggestSum = 0;
+        int biggestSum = -10000000;
         int curSum = 0;
         if (nums.length == 1) return nums[0];
-        int biggestNegative = biggestNegative(nums);
-        if (biggestNegative != 0) return biggestNegative;
+     
 
         for (int i = 0; i < nums.length; i++){
+            
             curSum += nums[i];
+
+            if (curSum > biggestSum){
+                biggestSum = curSum;
+            }
 
             if (curSum < 0){
                 curSum = 0;
                 continue;
             }
 
-            if (curSum > biggestSum){
-                biggestSum = curSum;
-            }
+
+            
+            
         }
         return biggestSum;
 
-    }
-
-    static int biggestNegative(int[] nums){
-        int biggestNeg = -1000000;
-        for(int i = 0; i < nums.length; i++){
-            if (nums[i] >= 0) return 0;
-            else if ( nums[i] > biggestNeg) biggestNeg = nums[i]; 
-        }
-
-        return biggestNeg;
     }
 }
