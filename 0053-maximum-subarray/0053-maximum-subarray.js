3d8e6457c6874cf79193c77c66a9309c488eaba8
G10H4rDy <132031977+G10H4rDy@users.noreply.github.com>
2024-10-15 15:25:41 -0400

    Time: 72 ms (56.63%), Space: 57.5 MB (80.52%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,46 +3,17 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
+    let biggestSum = -10000000;
+    let curSum = 0;
 
-    if (nums.length === 1){
-        return nums[0];
-    }
-    
-    // check if every elent in array is negative; if so, return the biggest element of them
-    const x = checkIfAllNegative(nums)
-    if (x) return x
-
-    let curSum = 0
-    let biggestSum = 0
-
-    for (let i = 0; i < nums.length; i++)
+    for (let i = 0 ; i < nums.length; i++)
     {
+        curSum += nums[i];
 
-        // Update current sum 
-        curSum += nums[i]
-
-        // if current sum is 0, all past elements and start counting from 0
-        if(curSum < 0 ) {
-            curSum = 0;
-            continue
-        }
-
-        // Keep track of biggest sum
         if (curSum > biggestSum) biggestSum = curSum;
 
-
+        if (curSum < 0) curSum = 0;
     }
-    
+
     return biggestSum;
-};
-
-
-const checkIfAllNegative = (nums) =>{
-    let biggestNegative = -10000;
-    for (const num of nums){
-        if (num >= 0) return false
-        else if(num > biggestNegative) biggestNegative = num
-    }
-
-    return biggestNegative
 }
