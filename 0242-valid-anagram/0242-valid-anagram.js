31635ec8a3c535df8936f7cb26af2162cc7047f2
G10H4rDy <132031977+G10H4rDy@users.noreply.github.com>
2024-10-22 16:09:44 -0400

    Time: 20 ms (96.67%), Space: 51 MB (86.13%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+
+    let letters1 = {};
+    let letters2 = {};
+
+    for (let i = 0; i < s.length; i++)
+    {
+        if(letters1[s[i]]){
+            letters1[s[i]]++;
+            continue;
+        }
+
+        letters1[s[i]] = 1;
+    }
+
+    for (let i = 0; i < t.length; i++)
+    {
+        if(letters2[t[i]]){
+            letters2[t[i]]++;
+            continue;
+        }
+
+        letters2[t[i]] = 1;
+    }
+
+
+    for (let letter in letters1) {
+         if (letters1[letter] !== letters2[letter]) return false;
+  }
+
+     for (let letter in letters2) {
+         if (letters1[letter] !== letters2[letter]) return false;
+  }
+
+
+  return true;
+}
