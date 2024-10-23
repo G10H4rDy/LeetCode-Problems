166921acff551673d3d081fc51326cc1be4da785
G10H4rDy <132031977+G10H4rDy@users.noreply.github.com>
2024-10-22 23:36:38 -0400

    Time: 0 ms (100%), Space: 48.7 MB (78.67%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+
+    // If last number is not 9
+    if(digits[digits.length - 1] !== 9){
+        digits[digits.length - 1]++;
+        return digits;
+    }
+
+    carry = 1;
+    for(let i = digits.length - 1; i >=0; i-- )
+    {
+        if( (digits[i] + carry) === 10){
+            digits[i] = 0;
+            if ( i === 0){
+                digits.unshift(1)
+                return digits
+            }
+            carry = 1;
+            continue
+        }
+
+        digits[i]++;
+        return digits
+    }
+};
