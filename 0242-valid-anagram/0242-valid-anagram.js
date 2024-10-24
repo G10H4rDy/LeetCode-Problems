5dbb1c213b4bf75079b80c04a7bb16ca00a1e420
G10H4rDy <132031977+G10H4rDy@users.noreply.github.com>
2024-10-23 20:44:36 -0400

    Time: 12 ms (98.58%), Space: 51.1 MB (83.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,37 +4,31 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-
     if(s.length !== t.length) return false;
 
     let letters1 = {};
     let letters2 = {};
 
-    for (let i = 0; i < s.length; i++)
-    {
+    for(let i = 0; i < s.length; i++){
         if(letters1[s[i]]){
             letters1[s[i]]++;
-            continue;
+            continue
         }
-
         letters1[s[i]] = 1;
     }
 
-    for (let i = 0; i < t.length; i++)
-    {
+    for(let i = 0; i < t.length; i++){
         if(letters2[t[i]]){
             letters2[t[i]]++;
-            continue;
+            continue
         }
-
         letters2[t[i]] = 1;
     }
 
+    for (let letter in letters1){
+        if(letters1[letter] !== letters2[letter]) return false;
+    }
 
-    for (let letter in letters1) {
-         if (letters1[letter] !== letters2[letter]) return false;
-  }
+    return true;
+};
 
-
-  return true;
-}
