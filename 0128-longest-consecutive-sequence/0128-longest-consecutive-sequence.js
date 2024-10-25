d59e62768cf7c931a87c093264e896a1cbaef376
G10H4rDy <132031977+G10H4rDy@users.noreply.github.com>
2024-10-24 22:07:13 -0400

    Time: 31 ms (99.31%), Space: 60 MB (93.61%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -5,8 +5,7 @@
 var longestConsecutive = function(nums) {
 
     if(nums.length <= 1) return nums.length;
-
-    nums = mergeSort(nums)
+    nums.sort((a,b) => a-b)
     let biggestSequence = 0;
     let sequence = 0;
     for(let i = 0, j = i + 1; j < nums.length; i++, j++){
@@ -21,26 +20,3 @@ var longestConsecutive = function(nums) {
 
     return biggestSequence + 1;
 };
-function mergeSort(arr) {
-  // Base case
-  if (arr.length <= 1) return arr
-  let mid = Math.floor(arr.length / 2)
-  // Recursive calls
-  let left = mergeSort(arr.slice(0, mid))
-  let right = mergeSort(arr.slice(mid))
-  return merge(left, right)
-}
-
-function merge(left, right) {
-  let sortedArr = [] // the sorted items will go here
-  while (left.length && right.length) {
-    // Insert the smallest item into sortedArr
-    if (left[0] < right[0]) {
-      sortedArr.push(left.shift())
-    } else {
-      sortedArr.push(right.shift())
-    }
-  }
-  // Use spread operators to create a new array, combining the three arrays
-  return [...sortedArr, ...left, ...right]
-}
